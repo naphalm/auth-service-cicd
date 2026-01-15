473c392b4e9df4b5b52d0e7170c16a5188ae8460
naphalm <master.sys.vp@gmail.com>
2026-01-15 13:46:27 +0200

    go app

---
+++ app/go.mod
@@ -0,0 +1,10 @@
+module auth-service
+
+go 1.22
+
+require (
+    github.com/gin-gonic/gin v1.10.0
+    github.com/golang-jwt/v5 v5.2.1
+    golang.org/x/crypto v0.20.0
+    github.com/lib/pq v1.10.9
+)
---
+++ app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"auth-service/src/db"
+	"auth-service/src/handlers"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.Init()
+
+	r:= gin.Default()
+
+	r.GET("/health", handlers.Health)
+	r.POST("/register", handlers.Register)
+	r.POST("/login", handlers.Login)
+	r.GET("validate", handlers.JWTAuthMiddleware(), handlers.Validate)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(r.Run(":" + port))
+}
